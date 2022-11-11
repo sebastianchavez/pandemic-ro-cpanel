5c78a2a2fa4dbdebcb323d2c0ee1e14f826f154c
Sebastián <seba.chavez21@gmail.com>
2022-11-11 02:57:23 -0300

    entity lock

--- src/locks/entities/lock.entity.ts
+++ src/locks/entities/lock.entity.ts
@@ -1,15 +1,15 @@
 export class Lock {
     account_id: number; // account_id para relacion entre tablas
-    isBgLock: boolean; // bloqueo de bg
-    startDateBgLock: Date; // desde
-    endDateBgLock: Date; // hasta, null = para siempre
-    isWoeLock: boolean; // bloqueo de mapas de woe
-    startDateWoeLock: Date; // desde
-    endDateWoeLock: Date; // hasta, null = para siempre
-    isBan: boolean; // baneo
-    startDateBan: Date;  // desde
-    endDateBan: Date; // hasta, null = para siempre
+    is_bg_lock: boolean; // bloqueo de bg
+    start_date_bg_lock: Date; // desde
+    end_date_bg_lock: Date; // hasta, null = para siempre
+    is_woe_lock: boolean; // bloqueo de mapas de woe
+    start_date_woe_lock: Date; // desde
+    end_date_woe_lock: Date; // hasta, null = para siempre
+    is_ban: boolean; // baneo
+    start_date_ban: Date;  // desde
+    end_date_ban: Date; // hasta, null = para siempre
     admin: string; // que admin realiza ultima operación
-    createdAt: Date; // fecha registro
-    updatedAt: Date; // fecha última actualización
+    created_at: Date; // fecha registro
+    updated_at: Date; // fecha última actualización
 }
